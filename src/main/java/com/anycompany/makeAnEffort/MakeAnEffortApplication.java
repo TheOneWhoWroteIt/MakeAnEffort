06c441c05f68840c6af1fcabef84c1746a657bab
st_nekto <deform@tut.by>
2020-09-27 20:35:52 +0300

    create project

---
+++ src/main/java/com/anycompany/makeAnEffort/MakeAnEffortApplication.java
@@ -0,0 +1,13 @@
+package com.anycompany.makeAnEffort;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MakeAnEffortApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MakeAnEffortApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/anycompany/makeAnEffort/controllers/WelcomeController.java
@@ -0,0 +1,13 @@
+package com.anycompany.makeAnEffort.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class WelcomeController {
+
+    @GetMapping("/welcome")
+    public String welcome(){
+        return "index";
+    }
+}
